b97c742321c0833e96e81e09e43326087fe39155
Michal Belák <michal.belak@datamole.cz>
2025-07-28 17:07:54 +0200

    feat: add LocalDeltaClient

---
+++ dtml/delta/local/client.py
@@ -0,0 +1,11 @@
+from dtml.delta.base_delta_client import BaseDeltaClient
+
+
+class LocalDeltaClient(BaseDeltaClient):
+    """
+    Local Delta client.
+    """
+
+    def _get_storage_options(self) -> dict[str, str]:
+        """Get the storage options for the Delta table."""
+        return {}
